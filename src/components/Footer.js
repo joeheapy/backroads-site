84a9da7e28400601dba6cb8f720f79cfa14c95d8
Joe Heapy <joeheapy@Joes-MacBook-Pro-2.local>
2024-12-24 12:04:24 +0000

    Updated links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,21 @@
 import { socialLinks } from '../data'
-import PageLink from './PageLink'
+import PageLinks from './PageLinks'
 import SocialLink from './SocialLink'
 
 const Footer = () => {
   return (
-    <>
-      {/* Iterate over pageLinks */}
-      <footer className="section footer">
-        {/* Iterate over pageLinks */}
-        <PageLink parentClass="footer-links" linkClass="footer-link" />
-
-        {/* Iterate over socialLinks */}
-        <ul className="footer-icons">
-          {socialLinks.map((link) => {
-            return (
-              <SocialLink key={link.id} {...link} itemClass="footer-icon" />
-            )
-          })}
-        </ul>
-
-        <p className="copyright">
-          copyright &copy; Backroads travel tours company
-          <span id="date">{new Date().getFullYear()}</span> all rights reserved
-        </p>
-      </footer>
-    </>
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass="footer-icon" />
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span>. all rights reserved
+      </p>
+    </footer>
   )
 }
 
